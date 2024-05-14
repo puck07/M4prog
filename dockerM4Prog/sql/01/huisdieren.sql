516dda00236b46a40f72b17701d8c70a161a895e
puck <36668@ma-web.nl>
2024-05-14 12:00:35 +0200

    add

---
+++ dockerM4Prog/sql/01/huisdieren.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: mariadb
+-- Gegenereerd op: 07 mei 2024 om 10:13
+-- Serverversie: 11.3.2-MariaDB-1:11.3.2+maria~ubu2204
+-- PHP-versie: 8.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `m4prog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `huisdieren`
+--
+
+CREATE TABLE `huisdieren` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `eigenaar` varchar(150) NOT NULL,
+  `typedier` varchar(150) NOT NULL,
+  `leeftijd` int(11) NOT NULL,
+  `geboortedatum` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `huisdieren`
+--
+ALTER TABLE `huisdieren`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `huisdieren`
+--
+ALTER TABLE `huisdieren`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ dockerM4Prog/sql/02/01 huisdierdata.sql
@@ -0,0 +1,9 @@
+INSERT INTO `huisdieren` (`id`, `name`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`) 
+VALUES 
+
+(NULL, 'puck', 'puck123', 'aap', '16', '2029-05-15'),
+(NULL, 'sander', 'sanderk123', 'kat', '9', '2019-09-15'),
+(NULL, 'ben', 'ben123', 'hond', '5', '2015-05-15'),
+(NULL, 'hans', 'hans123', 'leeuw', '7', '2014-07-13'),
+(NULL, 'emma', 'emma123','muis', '3', '2029-05-15'),
+(NULL, 'jan', 'jan123', 'hamster', '10', '2015-06-18');
